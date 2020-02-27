290011315e8d0bb0b00fba8dc4a5cbd20f426e86
Atreya Ray <atreyaray@gmail.com>
2020-02-27 09:25:05 +0200

    Created readFile method in the FileHandler Class (w/o testing)

--- src/FileHandler.scala
+++ src/FileHandler.scala
@@ -1,5 +1,35 @@
-
+import java.io._
+import scala.collection.mutable.Buffer
 
 class FileHandler {
   
+  def readFile (textFile : String) = {
+    
+    val fileReader = new FileReader(textFile)
+    val bufferedReader = new BufferedReader(fileReader)
+    
+    try{
+        var blocks = Buffer[Buffer[String]]()
+        var readLine = bufferedReader.readLine()
+        while (readLine != null){
+          
+          var block = Buffer[String]()
+          
+          if (readLine.charAt(0) == '#'){
+            while(!readLine.isEmpty()){
+              block += readLine
+              readLine = bufferedReader.readLine()
+            }
+          }
+          
+          readLine = bufferedReader.readLine()
+        }
+    }catch{
+      case e : IOException => println("Error found in readFile method : " + e)
+    }finally{
+      fileReader.close()
+      bufferedReader.close()
+    }
+    
+  }
 }
