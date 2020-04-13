d4214b9c89b5e5b32c985b9edc7c09831f965692
Atreya Ray <atreyaray@gmail.com>
2020-04-14 00:53:21 +0300

    FileHandler class updated
    loadGame method now updated. Method can load a game from any file.

--- src/cassino_game/FileHandler.scala
+++ src/cassino_game/FileHandler.scala
@@ -8,8 +8,8 @@ import java.util.Calendar
 class FileHandler {
   
   //reads a file and returns the chunks of data 
-  def readFile (textFile : String) : Array[String]= {
-    io.Source.fromFile(textFile).mkString.split("###")
+  def readFile (textFile : String) : Array[Array[String]]= {
+    io.Source.fromFile(textFile).mkString.split("###").map(_.trim()).map(_.split('\n'))
   }
   
   def saveGame(textFile : String ) = {
@@ -30,26 +30,86 @@ class FileHandler {
     
     //Block 2: Metadata
     //##round number, timer and turn need to be worked on.
-    val cardsOnTable = Game.cardsOnTable.map(_.name).foldLeft("")((m,n)=> m+n)
+    val cardsOnTable = Game.cardsOnTable.map(_.name).foldLeft("")((m,n)=> m+n+":")
     val lastCapturer = if(Game.lastCapturer.isDefined) Game.lastCapturer.get.name else "NONE"
-    val cardsInDeck = Game.cards.map(_.name).foldLeft("")((m,n)=>m+n)
-    writer.write("###\nMETADATA\nNUMBEROFPLAYERS "+ Game.players.length + "\nROUNDNUMMBER "+ 1 + "\nCARDSREMAINING " + Game.cardsOnTable.length
+    val cardsInDeck = Game.cards.map(_.name).foldLeft("")((m,n)=>m+n+":")
+    writer.write("###\nMETADATA\nNUMBEROFPLAYERS "+ Game.players.length + "\nROUNDNUMMBER "+ 1 + "\nNOOFCARDSREMAINING " + Game.cardsOnTable.length
                   + "\nCARDSONTABLE " + cardsOnTable + "\nCARDSINDECK " + cardsInDeck  +"\nLASTCAPTURER " + lastCapturer + "\n\n")
      
     //Block 3-N: Player Data
     for (i <- Game.players){
-      val playerNo = Game.players.indexOf(i)+1
-      val hand = i.cardsInHand.map(_.name).foldLeft("")((m,n)=>m+n)
-      val captured  = i.capturedCards.map(_.name).foldLeft("")((m,n)=> m+n)
-      writer.write("###\nPLR " + playerNo + "\nNAME " + i.name + "\nSCORE " + i.points + "\nHAND " + hand + "\nCAPTUREDCARDS " + captured + "\n\n")  
+      val hand = i.cardsInHand.map(_.name).foldLeft("")((m,n)=>m+n+":")
+      val captured  = i.capturedCards.map(_.name).foldLeft("")((m,n)=> m+n+":")
+      writer.write("###\nPLR" + "\nNAME " + i.name + "\nSCORE " + i.points + "\nHAND " + hand + "\nCAPTUREDCARDS " + captured + "\n\n")  
     }
     
     //EOF
     writer.write("###\nEOF")
     
+    //closing streams
     writer.close()
     fileWriter.close()
- 
+  }
+  
+  def loadGame (textFile : String) = {
+    //all the read data
+    val blocks = this.readFile(textFile)
     
+    //blocks of data containing metadata and player data
+    var metadata = Array[String]()
+    var playerdata = Array[Array[String]]()
+    
+    //parsing file to determine metadata and player data
+    for (currentBlock <- blocks){
+      val x = currentBlock
+      currentBlock(0) match{
+        case "" => 
+        case "METADATA" => metadata = currentBlock
+        case "PLR" => playerdata = playerdata :+ currentBlock 
+        case other => 
+      }
+    }
+    
+    //initalizing metadata
+    for (i <- metadata){
+        val metaBlocks = i.split(" ").toArray
+      metaBlocks(0) match{
+        case "CARDSONTABLE" =>  Game.cardsOnTable = Vector[Card]()
+                               if(metaBlocks.length > 1 ) metaBlocks(1).split(":").toArray.foreach(n => Game.cardsOnTable = Game.cardsOnTable :+ new Card(n) )
+        case "CARDSINDECK" => Game.cards = Vector[Card]()
+                               if(metaBlocks.length > 1 )  metaBlocks(1).split(":").toArray.foreach(n => Game.cards= Game.cards :+ new Card(n) )
+        case "LASTCAPTURER" => Game.lastCapturer =  if(metaBlocks(1) =="NONE") None else Some(new Player(metaBlocks(1)))
+        case other =>                                
+      }
+    }
+    
+    //initializing game data
+    for (i <- playerdata){
+      //
+      Game.players = Vector[Player]()
+      var name = ""
+      var score = 0 
+      var hand = Vector[Card]()
+      var capturedCards = Vector[Card]()
+      //iterates through different players
+      for(j <- i){
+        val data = j.split(" ")
+        //iterates through data of each player 
+        data(0) match{
+          case "NAME" =>  name = data(1)
+          case "SCORE" => score = data(1).toInt
+          case "HAND" => if(data.length > 1) hand = data(1).split(":").toVector.map(new Card(_))
+          case "CAPTUREDCARDS" => if(data.length > 1)  capturedCards = data(1).split(":").toVector.map(new Card(_))
+          case other => 
+        }
+      }
+      //adds newPlayer to list
+      val newPlayer = new Player(name)
+      newPlayer.points = score
+      newPlayer.cardsInHand = hand
+      newPlayer.capturedCards = capturedCards
+      println("New Player " + newPlayer)
+      Game.players = Game.players :+ newPlayer
+    }
   }
 }
